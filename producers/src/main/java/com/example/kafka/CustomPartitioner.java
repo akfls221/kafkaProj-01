89f788ba46b70c0906c424f46cd1569c720da3c8
엄태권 <akfls221@gmail.com>
2023-12-30 03:54:54 +0900

    feat : CustomPartitioner 추가

---
+++ producers/src/main/java/com/example/kafka/CustomPartitioner.java
@@ -0,0 +1,50 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.clients.producer.internals.StickyPartitionCache;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+public class CustomPartitioner implements Partitioner {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(CustomPartitioner.class.getName());
+    private final StickyPartitionCache stickyPartitionCache = new StickyPartitionCache();
+    private String specialKeyName;
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+        this.specialKeyName = configs.get("custom.specialKey").toString();
+    }
+
+    @Override
+    public int partition(String topic, Object key, byte[] keyBytes, Object value, byte[] valueBytes, Cluster cluster) {
+        List<PartitionInfo> partitionInfos = cluster.partitionsForTopic(topic);
+        int numPartitions = partitionInfos.size();
+        int numSpecialPartitions = (int) (numPartitions * 0.5);
+        int partitionIndex;
+
+        if (keyBytes == null) {
+            return stickyPartitionCache.partition(topic, cluster);
+        }
+
+        if (key.equals(specialKeyName)) {
+            partitionIndex = Utils.toPositive(Utils.murmur2(valueBytes)) % numSpecialPartitions;
+        } else {
+            partitionIndex = Utils.toPositive(Utils.murmur2(keyBytes)) % (numPartitions - numSpecialPartitions) + numSpecialPartitions;
+        }
+
+        LOGGER.info("key : {} is sent to partition: {}", key, partitionIndex);
+        return partitionIndex;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
