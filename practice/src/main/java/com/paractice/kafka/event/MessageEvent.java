93ece73503b5c60d3e84192aabaf36eae2ce5694
엄태권 <akfls221@gmail.com>
2024-01-02 00:20:39 +0900

    feat : MessageEvent 객체 추가

---
+++ practice/src/main/java/com/paractice/kafka/event/MessageEvent.java
@@ -0,0 +1,11 @@
+package com.paractice.kafka.event;
+
+public class MessageEvent {
+    public String key;
+    public String value;
+
+    public MessageEvent(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
